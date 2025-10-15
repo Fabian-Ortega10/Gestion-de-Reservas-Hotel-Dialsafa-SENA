53efc79a4a5506a7c1646dd25191ff7291c0727b
Fabian Ortega <nestorf92@gmail.com>
2025-10-14 19:54:05 -0500

    limpiar_datos.py
    Limpiar datos con el objetivo de evitar duplicidad

---
+++ limpiar_datos.py
@@ -0,0 +1,14 @@
+import json
+
+archivos = [
+    "datos/clientes.json",
+    "datos/habitaciones.json",
+    "datos/reservas.json",
+    "datos/pagos.json"
+]
+
+for archivo in archivos:
+    with open(archivo, "w", encoding="utf-8") as f:
+        json.dump([], f, ensure_ascii=False, indent=4)
+
+print("✅ Archivos limpiados correctamente.")
