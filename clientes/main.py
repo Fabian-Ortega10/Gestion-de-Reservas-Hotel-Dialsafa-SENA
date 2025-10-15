071b0d38d9269bbcd66cef6f053d17a82f551c26
Fabian Ortega <nestorf92@gmail.com>
2025-10-14 19:39:19 -0500

    Delete clientes directory

--- clientes/main.py
+++
@@ -1,9 +0,0 @@
-class Cliente:
-    def __init__(self, nombre, telefono):
-        self.nombre = nombre
-        self.telefono = telefono
-
-
-Diana = Cliente("Diana", 3124398432)
-
-print(f"{Diana.nombre} {Diana.telefono}")
